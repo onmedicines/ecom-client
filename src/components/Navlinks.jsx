fc64b075890253f9c136a1551f642c6e3e361742
onmedicines <semwalanurag26@gmail.com>
2024-11-10 12:34:54 +0530

    Remove useless border

--- src/components/Navlinks.jsx
+++ src/components/Navlinks.jsx
@@ -2,7 +2,7 @@ import { NavLink } from "react-router-dom";
 
 export default function Navlinks({ menuVisible = false, setMenuVisible = () => {} }) {
   return (
-    <ul className={menuVisible ? "border border-red-800 h-full flex flex-col items-center justify-around text-xl gap-8 py-16" : "flex"}>
+    <ul className={menuVisible ? "h-full flex flex-col items-center justify-around text-xl gap-8 py-16" : "flex"}>
       <li className="focus:ring" onClick={() => setMenuVisible(false)}>
         <NavLink to="/" className={({ isActive }) => (isActive ? "bg-zinc-800 text-white  py-2 px-3 flex items-center " : "py-2 px-3 flex items-center")}>
           Home
