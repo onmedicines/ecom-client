aff03957c18dcda64c267122c6f0bc636f600715
onmedicines <semwalanurag26@gmail.com>
2024-11-11 23:46:44 +0530

    Update home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,11 @@
 export default function HomePage() {
-  return <div className="h-full border-2 text-8xl grid place-items-center">HOME</div>;
+  return (
+    <div className="h-full text-center flex flex-col items-center lg:px-40 sm:px-28 sm:py-16">
+      <h1 className="text-2xl pb-6">About this project</h1>
+      <p className="text-lg">
+        I developed a responsive e-commerce web application using React and React Router, featuring dynamic routing for seamless navigation across product pages, cart, and error handling. Context API manages global cart state, allowing users to add, remove, and adjust item quantities. The app utilizes custom loader functions for asynchronous data fetching, ensuring efficient page load. Tailwind CSS is used for responsive design, including a hamburger menu for smaller screens. The project demonstrates my proficiency in React, React Router, and modern JavaScript practices like asynchronous
+        fetching.
+      </p>
+    </div>
+  );
 }
